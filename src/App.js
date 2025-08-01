edb249c10779d7cd947fbcbe87acb52a37e12363
sanath-kumara <dmsksanath@gmail.com>
2025-08-01 19:00:15 +0530

    Under construction content added

--- src/App.js
+++ src/App.js
@@ -1,22 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>🚧 Under Construction 🚧</h1>
+        <p>This site is currently being built. Please check back soon.</p>
       </header>
     </div>
   );
